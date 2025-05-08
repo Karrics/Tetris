8e26b2af2c637e7e224b98fd57c43df94178d4ae
Karrics <143880497+Karrics@users.noreply.github.com>
2025-05-08 13:05:46 +0300

    Add files via upload

---
+++ build/CMakeFiles/Tetris.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tetris.dir/src/Game.cpp.o"
+  "CMakeFiles/Tetris.dir/src/Game.cpp.o.d"
+  "CMakeFiles/Tetris.dir/src/Grid.cpp.o"
+  "CMakeFiles/Tetris.dir/src/Grid.cpp.o.d"
+  "CMakeFiles/Tetris.dir/src/InputHandler.cpp.o"
+  "CMakeFiles/Tetris.dir/src/InputHandler.cpp.o.d"
+  "CMakeFiles/Tetris.dir/src/Renderer.cpp.o"
+  "CMakeFiles/Tetris.dir/src/Renderer.cpp.o.d"
+  "CMakeFiles/Tetris.dir/src/Tetromino.cpp.o"
+  "CMakeFiles/Tetris.dir/src/Tetromino.cpp.o.d"
+  "CMakeFiles/Tetris.dir/src/main.cpp.o"
+  "CMakeFiles/Tetris.dir/src/main.cpp.o.d"
+  "Tetris"
+  "Tetris.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tetris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
