5920f36590af23a70be5d0352fb78f7cd942a6f2
Karrics <143880497+Karrics@users.noreply.github.com>
2025-06-09 08:48:53 +0300

    Delete build directory

--- build/CMakeFiles/TetrisTests.dir/TetrisTests_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/TetrisTests.dir/TetrisTests_autogen/mocs_compilation.cpp.o: \
-  /Users/karrics/Projects/Tetris_1/build/TetrisTests_autogen/mocs_compilation.cpp
--- build/CMakeFiles/TetrisTests.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for TetrisTests.
-# This may be replaced when dependencies are built.
